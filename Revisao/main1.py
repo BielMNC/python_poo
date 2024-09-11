4e111dd7bb4bed137a94c220c53f9e2c2afec726
BielMNC <gaabrieljesus@hotmail.com>
2024-09-11 11:12:31 -0300

    exercicios de revisao

--- Revisao/main1.py
+++ Revisao/main1.py
@@ -1,9 +1,9 @@
 from quiz1 import *
 
 if __name__ == '__main__':
-    q1 = Quiz('POO','Rapahel', 10, 2)
-    q2 = Quiz2A('POO','Thalyta',5,7)
-    q3 = Quiz3A('POO', 'Thais',11,1)
+    q1 = Quiz(10, 2)
+    q2 = Quiz2A(5,7)
+    q3 = Quiz3A(11,1)
 
     print(q1) # QUIZ
     print(q2) # QUIZ2A
@@ -13,3 +13,13 @@ if __name__ == '__main__':
     print(q1.get_acertos())
     print(q1.get_erros())
     print(q1.calcular_pontos())
+
+    lista_qs = [q1,q2,q3]
+    a1 = Aluno(1, 'Raphael', lista_qs)
+    a2 = Aluno(2, 'Thalyta', lista_qs)
+    print(a1)
+
+    d = Disciplina("POO", "Ferauche", 2024, 4)
+    d.add_aluno(a1)
+    d.add_aluno(a2)
+    print(d)
--- Revisao/quiz1.py
+++ Revisao/quiz1.py
@@ -1,12 +1,9 @@
+from typing import List
 class Quiz:
-    disciplina : str
-    aluno : str
     __acertos : int
     __erros : int
 
-    def __init__(self, d : str, a : str, acertos : int, erros : int):
-        self.disciplina = d
-        self.aluno = a
+    def __init__(self, acertos : int, erros : int):
         self.__acertos = acertos
         self.__erros = erros
 
@@ -21,9 +18,7 @@ class Quiz:
 
 
     def __str__(self):
-        info = f'Disciplina: {self.disciplina}\n'
-        info += f'Aluno: {self.aluno}\n'
-        info += f'Acertos {self.__acertos} Erros: {self.__erros}\n'
+        info = f'Acertos {self.__acertos} Erros: {self.__erros}\n'
         info += f'Total: {self.calcular_pontos()}'
         return info
 
@@ -36,3 +31,53 @@ class Quiz2A(Quiz):
 class Quiz3A(Quiz):
     def calcular_pontos(self):
         return self.get_acertos() - 2*(self.get_erros())
+
+class Aluno:
+    __matricula : int
+    __nome : str
+    __quizes : List[Quiz]
+
+    def __init__(self, mat : int, nom : str, kahoots : List[Quiz]):
+        self.__matricula = mat
+        self.__nome = nom
+        self.__quizes = kahoots
+
+    def __str__(self):
+        info = f'Matricula: {self.__matricula}\n'
+        info += f'Nome: {self.__nome}\n'
+        soma = 0
+
+        for q in self.__quizes:
+            soma += q.calcular_pontos()
+        info += f'Total de Pontos {soma}\n'
+        return info
+
+    def get_matricula(self):
+        return self.__matricula
+
+
+class Disciplina:
+    __nome : str
+    __professor: str
+    __ano : int
+    __semestre : int
+    __alunos : List[Aluno] = []
+
+    def __init__(self, n : str, p : str, ano : int, sem : int):
+        self.__nome = n
+        self.__professor = p
+        self.__semestre = sem
+        self.__ano = ano
+
+    def add_aluno(self, a : Aluno):
+        for aluno in self.__alunos:
+            if aluno.get_matricula() == a.get_matricula():
+                raise Exception('Aluno ja existe! ')
+        self.__alunos.append(a)
+
+    def __str__(self):
+        info = f'Discciplina: {self.__nome} Professor: {self.__professor}\n'
+        info += f'Ano: {self.__ano} Semestre: {self.__semestre}\n'
+        for aluno in self.__alunos:
+            info += f'{aluno}'
+        return info
